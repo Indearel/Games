c69d7a8fbd68e6f0f67f040948b2dacc2c305f17
cyberindygos <cyberindygos@gmail.com>
2022-07-11 22:15:02 +0200

    Refactoring

--- 8OutOf10/main.py
+++ 8OutOf10/main.py
@@ -4,7 +4,7 @@ import numbers
 def main():
    choice = input('What do we have today, "l" - letter, "n" - numbers\n')
    if choice == "l":
-      letters.letters_generarator()
+      letters.letters_generator()
    elif choice == "n":
       numbers.numbers_generator()
 
